12b7ac501d2b682f2fadc0d01c61da053acada76
sqmasep <78935965+sqmasep@users.noreply.github.com>
2022-11-09 23:35:34 +0100

    query test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@mui/material";
 import React from "react";
+import trpc from "../trpc";
 
 const Home: React.FC = () => {
-  return <Button>epic</Button>;
+  const res = trpc.user.test.useQuery();
+  return (
+    <>
+      <pre>{JSON.stringify(res, null, 2)}</pre>
+
+      <Button>epic</Button>
+    </>
+  );
 };
 
 export default Home;
